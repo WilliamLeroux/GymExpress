6db82228e1b705f791f6139f6a17adb0f22e57b6
William Leroux <2251513@etu.cegep-lanaudiere.qc.ca>
2025-01-27 12:48:02 -0500

    Structure

---
+++ GymExpress/GymExpress/Views/Utils/SideBars/SideBarAdmin.swift
@@ -0,0 +1,14 @@
+//
+//  SideBarAdmin.swift
+//  GymExpress
+//
+//  Created by William Leroux on 2025-01-27.
+//
+
+import SwiftUI
+
+extension View {
+    var sideBarAdmin: some View {
+        Text("Admin")
+    }
+}
---
+++ GymExpress/GymExpress/Views/Utils/SideBars/SideBarTrainer.swift
@@ -0,0 +1,14 @@
+//
+//  SideBarTrainer.swift
+//  GymExpress
+//
+//  Created by William Leroux on 2025-01-27.
+//
+
+import SwiftUI
+
+extension View {
+    var sideBarTrainer: some View {
+        Text("Trainer")
+    }
+}
