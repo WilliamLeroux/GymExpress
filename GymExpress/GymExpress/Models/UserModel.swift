44299b5071960787ada246cfabf6d59a2311c51b
Nicolas Morin <nickdrum@live.ca>
2025-02-07 12:25:29 -0500

    Ajout des models
    Exercises, user, workout, appointment

---
+++ GymExpress/GymExpress/Models/UserModel.swift
@@ -0,0 +1,20 @@
+//
+//  UserModel.swift
+//  GymExpress
+//
+//  Created by Nicolas Morin on 2025-02-07.
+//
+
+import Foundation
+
+/// Strucutre d'un utilisateur
+struct UserModel {
+    let id: Int /// Identifiant
+    var name: String /// Prénom
+    var lastName: String /// Nom
+    var email: String /// Adresse courriel
+    var password: String /// Mot de passe
+    let type: UserType /// Type d'utilisateur
+    var membership: MembershipData? /// Abonnement
+    var salary: Double? /// Salaire
+}
