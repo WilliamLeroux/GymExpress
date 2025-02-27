7ddb5edc5ba1e073ce5c047506e94e6e1e806146
William Leroux <2251513@etu.cegep-lanaudiere.qc.ca>
2025-02-27 08:58:22 -0500

    WorkoutFrequenceController

--- GymExpress/GymExpress/Controllers/Client/WorkoutFrequenceController.swift
+++ GymExpress/GymExpress/Controllers/Client/WorkoutFrequenceController.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 class WorkoutFrequenceController: ObservableObject {
     static let shared = WorkoutFrequenceController()
+    private let dbManager = DatabaseManager.shared
+    private var frequences: [FrequenceModel] = []
+    
+    @Published var week: [Day] = []
     
     private init() {}
     
-    
+    private func loadInitialData() {
+        //frequences = dbManager.fetchDatas(request: Request.select(table: DbTable.frequence, columns: ["id", "user_id", "date"], condition: <#T##String#>), params: [])
+    }
     
     func addPresence(date: Date) {
         
