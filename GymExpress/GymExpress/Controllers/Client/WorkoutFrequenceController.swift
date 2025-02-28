4c1f87b557c3bd05b2d90c30271d953947df0c68
William Leroux <2251513@etu.cegep-lanaudiere.qc.ca>
2025-02-28 10:57:38 -0500

    Ajout - WorkoutFrequenceController

--- GymExpress/GymExpress/Controllers/Client/WorkoutFrequenceController.swift
+++ GymExpress/GymExpress/Controllers/Client/WorkoutFrequenceController.swift
@@ -10,17 +10,66 @@ import SwiftUI
 class WorkoutFrequenceController: ObservableObject {
     static let shared = WorkoutFrequenceController()
     private let dbManager = DatabaseManager.shared
+    private let calendar = Calendar.current
     private var frequences: [FrequenceModel] = []
+    private var currentMonth: Int
+    private var currentYear: Int
+    @Published var currentDate: Date = Date()
+    @Published var month: [Day] = []
     
-    @Published var week: [Day] = []
-    
-    private init() {}
+    private init() {
+        currentMonth = calendar.component(.month, from: Date(timeIntervalSinceNow: 0))
+        currentYear = calendar.component(.year, from: Date(timeIntervalSinceNow: 0))
+        loadInitialData()
+        month = [Day(name: "Dimanche", count: 0), Day(name: "Lundi", count: 0), Day(name: "Mardi", count: 0), Day(name: "Mercredi", count: 0), Day(name: "Jeudi", count: 0), Day(name: "Vendredi", count: 0), Day(name: "Samedi", count: 0)]
+        loadData()
+    }
     
     private func loadInitialData() {
-        //frequences = dbManager.fetchDatas(request: Request.select(table: DbTable.frequence, columns: ["id", "user_id", "date"], condition: <#T##String#>), params: [])
+        frequences = dbManager.fetchDatas(request: Request.select(table: DbTable.frequence, columns: ["id", "user_id", "date"], condition: "WHERE user_id = ?"), params: [LoginController.shared.currentUser?.id ?? -1]) ?? []
+    }
+    
+    private func loadData() {
+        for i in 0..<month.count {
+            month[i].count = 0
+        }
+        frequences.forEach { (freq) in
+            if freq.date != nil {
+                if calendar.component(.month, from: freq.date!) == currentMonth && calendar.component(.year, from: freq.date!) == currentYear {
+                    month[calendar.component(.weekday, from: freq.date!) - 1].count += 1
+                }
+            }
+        }
     }
     
     func addPresence(date: Date) {
-        
+        let freq = FrequenceModel(userId: LoginController.shared.currentUser?.id ?? -1, date: date, presence: true)
+        let success = dbManager.insertData(request: Request.createFrequence, params: freq)
+        if success {
+            frequences.append(freq)
+            loadData()
+        }
+    }
+    
+    func increaseMonth() {
+        if currentMonth == 12 {
+            currentMonth = 1
+            currentYear += 1
+        } else {
+            currentMonth += 1
+        }
+        currentDate = calendar.date(byAdding: .month, value: 1, to: currentDate)!
+        loadData()
+    }
+    
+    func decreaseMonth() {
+        if currentMonth == 1 {
+            currentMonth = 12
+            currentYear -= 1
+        } else {
+            currentMonth -= 1
+        }
+        currentDate = calendar.date(byAdding: .month, value: -1, to: currentDate)!
+        loadData()
     }
 }
--- GymExpress/GymExpress/Models/FrequenceModel.swift
+++ GymExpress/GymExpress/Models/FrequenceModel.swift
@@ -6,20 +6,57 @@
 //
 
 import Foundation
+import SQLite3
 
-struct FrequenceModel: Identifiable, SQLConvertable {
+struct FrequenceModel: Identifiable, SQLConvertable, InitializableFromSQLITE {
     var id: Int = -1
     var userId: Int = -1
     var date: Date? = nil
+    var presence: Bool = false
     
-    init(userId: Int, date: Date? = nil) {
+    init(userId: Int, date: Date? = nil, presence: Bool = false) {
         self.userId = userId
         self.date = date
+        self.presence = presence
     }
     
-    
+    init(from pointer: OpaquePointer?) {
+        guard let pointer = pointer else {
+            return
+        }
+        
+        let columnCount = sqlite3_column_count(pointer)
+        var columnIndex: Int32 = 0
+        for i in 0..<columnCount {
+            columnIndex = Int32(DatabaseManager.shared.tableMaps[1].firstIndex(of: String(cString: sqlite3_column_name(pointer, i)!)) ?? 0)
+            
+            switch columnIndex {
+            case 1:
+                self.id = Int(sqlite3_column_int(pointer, Int32(i)))
+            case 2:
+                self.userId = Int(sqlite3_column_int(pointer, Int32(i)))
+            case 3:
+                if let dateString = sqlite3_column_text(pointer, i) {
+                    
+                    let dateStr = String(cString: dateString)
+                    if dateStr.count(where: {$0 == "-"}) != 2 {
+                        self.date = DateUtils.shared.formatter.date(from: dateStr)!
+                    } else {
+                        self.date = DateUtils.shared.formatterSimpleDate.date(from: dateStr)!
+                    }
+                    
+                } else {
+                    self.date = nil
+                }
+            case 4:
+                self.presence = sqlite3_column_int(pointer, Int32(i)) == 1
+            default:
+                break
+            }
+        }
+    }
     
     var params: [Any] {
-        return [userId, date as Any]
+        return [userId, date as Any, presence]
     }
 }
