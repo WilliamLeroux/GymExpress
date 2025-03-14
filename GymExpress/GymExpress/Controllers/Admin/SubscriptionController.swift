a4d1616909f351e7d2cd24ed0049460d69e357cb
Nicolas Morin <nickdrum@live.ca>
2025-03-13 20:00:11 -0400

    Ajout du controller subscription

---
+++ GymExpress/GymExpress/Controllers/Admin/SubscriptionController.swift
@@ -0,0 +1,12 @@
+//
+//  SubscriptionController.swift
+//  GymExpress
+//
+//  Created by Nicolas Morin on 2025-03-13.
+//
+
+import SwiftUI
+
+class SubscriptionController {
+    @Published var memberships: [MembershipData] = []
+}
